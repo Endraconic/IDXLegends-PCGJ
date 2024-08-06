a4310b5147adf09d64873a6006557e9adc9aaf32
Endraconic <guppieking@gmail.com>
2024-08-06 18:23:32 -0400

    variable enemy types
    added variable enemy types
    
    see concept doc for details

--- Killbasa/Assets/Scripts/KillTarget.cs
+++ Killbasa/Assets/Scripts/KillTarget.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 
 public class KillTarget : MonoBehaviour
 {
+
+    public enum EnemyType { Fatty, Lean, Rich }
+    public EnemyType enemyType;
+
+    public float fattyLifespan;
+    public float leanLifespan;
+    public float richLifespan;
+
+    public float leanChance;
+    public float richChance;
+
+    public int direction;
+    public float movementSpeed;
+
+
     public KillGameManager manager;
     public float lifespan;
     public bool isCop = false;
+
+
 
     private float lifeTimer = 0.0f;
 
@@ -17,11 +34,52 @@ public class KillTarget : MonoBehaviour
             GetComponent<SpriteRenderer>().color = Color.red;
         else
             GetComponent<SpriteRenderer>().color = Color.green;
+
+        //set enemy type here
+        float r = Random.Range(0.0f, 100.0f);
+        if (r < richChance)
+        {
+            enemyType = EnemyType.Rich;
+            lifespan = richLifespan;
+        }
+        else if (r < leanChance)
+        {
+            enemyType = EnemyType.Lean;
+            direction = Random.Range(0, 2) * 2 - 1;
+            movementSpeed = 1.0f;
+
+            lifespan = leanLifespan;
+        }
+        else
+        {
+            enemyType = EnemyType.Fatty;
+            lifespan = fattyLifespan;
+        }
+
     }
 
     private void Update()
     {
         lifeTimer += Time.deltaTime;
+
+        switch (enemyType)
+        {
+            case EnemyType.Rich:
+
+                break;
+
+            case EnemyType.Lean:
+
+                transform.position += new Vector3(direction, 0, 0) * Time.deltaTime * movementSpeed;
+
+                break;
+
+            case EnemyType.Fatty:
+                transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+                break;
+        }
+
+
 
         if (lifeTimer > lifespan)
             Destroy(gameObject);
